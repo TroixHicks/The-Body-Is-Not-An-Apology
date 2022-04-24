f831bfd287a74985e7ebdf2e06896ff609cfa9cf
samekh_resh <C0d1ng4Lyf3>
2022-04-24 12:46:49 -0400

    made changes to homepage and save page, attempting sonya auth

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,4 @@
-module.exports = function(app, passport, db, multer, ObjectId) {
+module.exports = function(app, passport, db, multer, ObjectId, sonyaEntry) {
 
    //MULTER =======================================================================
    const storage = multer.diskStorage({
@@ -111,6 +111,22 @@ app.post('/sonyasPosts', (req, res) => {
         res.redirect('/homePage')
       })
     })
+
+
+// routes for individual posting pages ==============================================================
+app.get('/indivSonyaPost', isLoggedIn, function (req, res) {
+
+    let postId = req.query.id
+    console.log('postid =', postId, req)
+    db.collection('sonyasPostings').findOne({ _id: ObjectId(postId) }, (err, result) => {
+      if (err) return console.log(err)
+        console.log(result)
+        res.render('indivSonyaPost.ejs', {
+          user: req.user,
+          sonyasPostings: result
+        })
+      })
+  });
 //routes for homepage ==============================================================
 
 app.get('/homePage', isLoggedIn, function(req, res) {
@@ -118,14 +134,42 @@ app.get('/homePage', isLoggedIn, function(req, res) {
           if (err) return console.log(err)
           res.render('homePage.ejs', {
             user : req.user,
-            // messages: result,
             sonyasPostings: result,
           })
         })
     });
 
-    // would we consider making another collection for sonja to post things? i think it would be easier than posting it all in one collection and having to have validation logic in the server side!. 
+// users route to save post
 
+//saving savesonya post
+
+app.put('/saveSonyaPost', isLoggedIn, (req, res) => {
+    db.collection('sonyasPostings')
+      .findOneAndUpdate({ _id: ObjectId(req.body.postId) }, {
+        $addToSet: {
+          interestedUsers: ObjectId(req.user.id)
+        }
+      }, {
+        sort: { _id: -1 },
+        upsert: false
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+  })
+
+  // rendering her posts on a page
+
+  app.get('/savedPosts', isLoggedIn, function (req, res) {
+    db.collection('sonyasPostings').find({interestedUsers: ObjectId(req.user.id)}).toArray((err, result) => {
+      if (err) return console.log(err)
+      console.log(result)
+      res.render('savedPosts.ejs', {
+        user : req.user,
+        sonyasPostings: result,
+      })
+    })
+  });
 
     
 
@@ -185,4 +229,14 @@ function isLoggedIn(req, res, next) {
         return next();
 
     res.redirect('/');
+}
+
+// route middleware to validate if it's sonya so only she can post
+function isSonyaTheUser(req, res, next) {
+//logic that checks if the user's id is correctly matching sonyas
+  if (req.user.email == sonyaEntry)
+//allows them to continue with where they were trying to do. 
+    return next();
+//will redirect user to profile if they are not sonya
+  res.redirect('/profile');
 }
--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
 var thumbDown = document.getElementsByClassName("fa-thumbs-down");
+var saveBtn = document.getElementsByClassName("fa-plus")
 var trash = document.getElementsByClassName("fa-trash");
 
 Array.from(thumbUp).forEach(function(element) {
@@ -48,6 +49,30 @@ Array.from(thumbDown).forEach(function(element) {
       window.location.reload(true)
     })
   });
+});
+
+Array.from(saveBtn).forEach(function(element) {
+      element.addEventListener('click', function(){
+
+        // const name = this.parentNode.parentNode.childNodes[1].innerText
+        // const msg = this.parentNode.parentNode.childNodes[3].innerText
+        // const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        console.log("this is the elements id: " + element.dataset.id)
+        fetch('saveSonyaPost', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            postId: element.dataset.id
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
 });
 
 Array.from(trash).forEach(function(element) {
